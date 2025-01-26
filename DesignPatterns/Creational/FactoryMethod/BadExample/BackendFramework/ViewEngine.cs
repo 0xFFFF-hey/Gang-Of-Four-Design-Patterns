b1813eadd2c2e94d7acf71d4e6673b72c6b828a8
helyousfi <visioncomputer200@gmail.com>
2025-01-26 20:47:10 +0100

    creational patterns

---
+++ DesignPatterns/Creational/FactoryMethod/BadExample/BackendFramework/ViewEngine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.FactoryMethod.BadExample.BackendFramework
+{
+    internal interface ViewEngine
+    {
+        string Render(string fileName, Dictionary<string, object> data);
+    }
+}
---
+++ DesignPatterns/Creational/FactoryMethod/BadExample/BladeView/BladeViewEngine.cs
@@ -0,0 +1,17 @@
+﻿using DesignPatterns.Creational.FactoryMethod.BadExample.BackendFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.FactoryMethod.BadExample.BladeView
+{
+    internal class BladeViewEngine : ViewEngine
+    {
+        public string Render(string fileName, Dictionary<string, object> data)
+        {
+            return "View rendered from " + fileName + " by Blade";
+        }
+    }
+}
