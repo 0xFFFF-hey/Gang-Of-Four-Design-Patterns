6d09893213fd6a982b9c1eb42530169d1639d94e
helyousfi <visioncomputer200@gmail.com>
2025-02-01 23:34:37 +0100

    SOLID : S bad example

---
+++ SOLID/SingleResponsibility/BadExample/OrderProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.SingleResponsibility.BadExample
+{
+    internal class OrderProcessor
+    {
+        public void ProcessOrder(Order order)
+        {
+            // Process the order (e.g., payment, inventory)
+            Console.WriteLine("Order processed");
+
+            // Send confirmation email
+            SendConfirmationEmail(order);
+        }
+
+        private void SendConfirmationEmail(Order order)
+        {
+            // Logic for sending an email
+            Console.WriteLine("Email sent to " + order.CustomerEmail);
+        }
+    }
+}
