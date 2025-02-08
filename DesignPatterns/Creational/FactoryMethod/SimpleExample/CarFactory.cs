5097c66fb6abfea306754e07a61cb39e1b9c9fe2
helyousfi <visioncomputer200@gmail.com>
2025-02-08 19:35:57 +0100

    FactoryMethod Simple example

---
+++ DesignPatterns/Creational/FactoryMethod/SimpleExample/CarFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.FactoryMethod.SimpleExample
+{
+    public class CarFactory : VehicleFactory
+    {
+        public override IVehicle CreateVehicle()
+        {
+            return new Car(); // Creates a Car
+        }
+    }
+}
---
+++ DesignPatterns/Creational/FactoryMethod/SimpleExample/TruckFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.FactoryMethod.SimpleExample
+{
+    public class TruckFactory : VehicleFactory
+    {
+        public override IVehicle CreateVehicle()
+        {
+            return new Truck(); // Creates a Truck
+        }
+    }
+}
